e7205a70bfd4cd5a0f913e475f0d1667467a87c6
enzoCorrea123 <enzorufino1508@gmail.com>
2023-10-31 21:51:09 -0300

    lista de listas bugada, mas funciona

--- my-app/src/ButtonCreate/ButtonCreate.js
+++ my-app/src/ButtonCreate/ButtonCreate.js
@@ -1,13 +1,48 @@
 import { View, Text, Pressable,StyleSheet, Button } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import metadata from "../storage.metadata.json";
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useMemo, useState } from "react";
+const buttonCreate = ({ route, navigation }) => {
+    const nameList = route.params;
+    const [listOfLists, setListOfLists] = useState([]);
 
-const buttonCreate = ({ navigation })=>{
-    return(
+    const focus = useIsFocused();
+
+    useEffect(() => {
+        getUserName();
+    }, []);
+
+    useEffect(() => {
+        setListOfLists(nameList);
+    }, [focus]);
+
+    const getUserName = async () => {
+        const listName = await AsyncStorage.getItem(metadata.LIST.LISTNAME);
+        if (listName) {
+            setListOfLists(JSON.parse(listName));
+        }
+    }
+
+    const printList = useMemo(() => {
+        if (listOfLists && listOfLists.length > 0) {
+            return listOfLists.map((list, index) => (
+                <View key={index} style={styles.components}>
+                    <Text>{list}</Text>
+                    <Text>15/08/2005 19:00</Text>
+                </View>
+            ));
+        }
+    },[listOfLists]);
+
+    return (
         <View>
-            <Pressable onPress={()=>{navigation.navigate("CriarLista")}} style={styles.button}>
+            <Pressable onPress={() => { navigation.navigate("CriarLista") }} style={styles.button}>
                 <Text>Criar lista</Text>
             </Pressable>
+            {printList}
         </View>
-    )
+    );
 }
 const styles = StyleSheet.create({
     button: {
@@ -19,5 +54,17 @@ const styles = StyleSheet.create({
         alignItems: "center",
         borderRadius: 10,
     },
+    components:{
+        flex: 1,
+        flexDirection: 'row',
+        gap: 10,
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        width: 300,
+        padding: 10,
+        borderRadius: 10,
+        
+    }
 })
 export default buttonCreate;
--- my-app/src/CreateList/CreateList.js
+++ my-app/src/CreateList/CreateList.js
@@ -1,12 +1,34 @@
-import { useState } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native"
-
-const createList = ()=>{
+import { useEffect, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import metadata from "../storage.metadata.json";
+const createList = ({navigation})=>{
     const [name, setName] = useState("");
+    const [listOfLists, setListOfLists] = useState([]);
+    useEffect(() => { saveUserName() }, [name]);
+
+    const saveUserName = async () => {
+        const saveName = [name] || [];
+        await AsyncStorage.setItem(metadata.LIST.LISTNAME, JSON.stringify(saveName));
+        console.log('foi')
+    }
+
+    // const handleInput = ()=>{
+    //     console.log('foi')
+    //     const list = [...listOfLists];
+    //     list.push(name);
+    //     setListOfLists(list);
+    //     navigation.navigate("Lista")
+    //     console.log(list)
+    // }
+
     return (
         <View>
             <Text>Adicionar uma nova lista</Text>
             <TextInput placeholder="Nome da lista" value={name} onChangeText={setName} style={styles.input}/>
+            <Pressable onPress={()=>{navigation.navigate("Lista", {nameList: name})}}>
+                <Text>Confirmar</Text>
+            </Pressable>
         </View>
     )
 }
